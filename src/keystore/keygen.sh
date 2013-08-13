da47284c087cd46a1da84c6d2446bf2024afe028
DY Kim <dykim@iniline.co.kr>
2013-08-13 15:53:03 +0900

    delete

--- src/keystore/keygen.sh
+++
@@ -1,3 +0,0 @@
-rm debug.keystore
-
-keytool -genkey -v -keystore debug.keystore -alias iniline -keyalg RSA -VALIDITY 10000
--- src/sign.sh
+++
@@ -1,4 +0,0 @@
-jarsigner -verbose -keystore ./keystore/debug.keystore -storepass iniline -keypass iniline \
-	-signedjar ./bin/Moojigae-release-signed.apk \
-	./bin/Moojigae-release-unsigned.apk \
-	iniline
