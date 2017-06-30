abc57ec69b6e6a5bbb2c45d554d10917dd78288f
dykim <panicstyle@gmail.com>
2017-06-30 13:39:29 +0900

    주민전용 홈페이지 주소 변경에 따른 수정 및 3.8.0 릴리즈

--- app/src/main/java/com/panicstyle/Moojigae/GlobalConst.java
+++ app/src/main/java/com/panicstyle/Moojigae/GlobalConst.java
@@ -4,6 +4,6 @@ package com.panicstyle.Moojigae;
  * Created by dykim on 2016. 3. 1..
  */
 public class GlobalConst {
-    public static String m_strServerName = "www.moojigae.or.kr";
+    public static String m_strServerName = "jumin.moojigae.or.kr";
     public static String m_strServer = "http://" + m_strServerName;
 }
